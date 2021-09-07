f3008977a2a14577cae16bc9a92ed7f77e6c15df
SimplyyJoji <59666562+SimplyyJoji@users.noreply.github.com>
2021-09-06 18:48:39 -0700

    finished about section and locations. need to add hover effect

--- hotel/src/components/Main.js
+++ hotel/src/components/Main.js
@@ -17,19 +17,35 @@ import imgSvg from '../imgs/flight.svg';
 function Main() {
   return (
     <>
+    <section id="about">
+    <Container fluid="md">
+      <Row className="more evenMorePad">
+        <Col md="auto mx-8"><Image className="svg " src={imgSvg} roundedCircle /></Col>
+        <Col md="auto mx-4">
+          <figure className="top">
+            <blockquote class="blockquote">
+              <p>“Travel is about the gorgeous feeling of teetering in the unknown.”</p>
+            </blockquote>
+          <figcaption class="blockquote-footer">
+          Anthony Bourdain <cite title="Source Title">Parts Unknown</cite>
+          </figcaption>
+          </figure></Col>
+      </Row>
+    </Container>
+    </section>
     <section id="places">
     <Container fluid="md">
-      <Row clasName="pb-5">
-        <Col md="auto"><Image className="locations" src={sf} roundedCircle /></Col>
-        <Col md="auto"><Image className="locations" src={seattle} roundedCircle /></Col>
-        <Col md="auto"><Image className="locations" src={chicago} roundedCircle /></Col>
-        <Col md="auto"><Image className="locations" src={ny} roundedCircle /></Col>
+      <Row className="morePad smallTop">
+        <Col md="auto mx-3"><Image className="locations" src={sf} roundedCircle /></Col>
+        <Col md="auto mx-3"><Image className="locations" src={seattle} roundedCircle /></Col>
+        <Col md="auto mx-3"><Image className="locations" src={chicago} roundedCircle /></Col>
+        <Col md="auto mx-3"><Image className="locations" src={ny} roundedCircle /></Col>
       </Row>
-      <Row clasName="pt-5">
-        <Col md="auto"><Image className="locations" src={singapore} roundedCircle /></Col>
-        <Col md="auto"><Image className="locations" src={dubai} roundedCircle /></Col>
-        <Col md="auto"><Image className="locations" src={tokyo} roundedCircle /></Col>
-        <Col md="auto"><Image className="locations" src={tokyo} roundedCircle/></Col>
+      <Row className="smallBot mt-3">
+        <Col md="auto mx-3"><Image className="locations" src={singapore} roundedCircle /></Col>
+        <Col md="auto mx-3"><Image className="locations" src={dubai} roundedCircle /></Col>
+        <Col md="auto mx-3"><Image className="locations" src={tokyo} roundedCircle /></Col>
+        <Col md="auto mx-3"><Image className="locations" src={tokyo} roundedCircle/></Col>
       </Row>
     </Container>
     </section>
