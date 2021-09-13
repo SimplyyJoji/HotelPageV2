b66b8675ffb77e661d13bb431a3367fef59c4d5f
SimplyyJoji <59666562+SimplyyJoji@users.noreply.github.com>
2021-09-12 17:06:01 -0700

    Added Navbar

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -1,10 +1,12 @@
 import Main from './components/Main'
+import Nav from './components/Nav';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Nav />
       <Main />
       <Footer />
     </div>
